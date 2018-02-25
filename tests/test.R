3b541a9953483cca15332b1c24dbc7d441a98c67
Roger-Li <liyz06@gmail.com>
2018-02-25 01:29:21 -0500

    update test case

--- tests/test.R
+++ tests/test.R
@@ -1,6 +1,3 @@
 library(testthat)
 
 expect_that(farsPackage::make_filename(2014),equals("accident_2014.csv.bz2"))
-
-expect_that(colnames(farsPackage::fars_summarize_years(c(2013, 2014, 2015))),
-            equals(c("MONTH","2013","2014","2015" )))
